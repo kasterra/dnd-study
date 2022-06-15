cc811f3f00344c0a58b326075a808c29336a61b6
kasterra <kasterra@naver.com>
2022-06-15 20:59:00 +0900

    ✨ feat: drag and drop result persistence

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from "react";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import Column from "./components/Column";
 import initialData from "./initial-data";
 interface IData {
@@ -14,9 +14,38 @@ interface IData {
 
 function App() {
   const [data, setData] = useState<IData>(initialData);
-  const onDragEnd = useCallback((result: any) => {
-    // TODO: reorder our column
-  }, []);
+  const onDragEnd = useCallback(
+    (result: DropResult) => {
+      const { destination, source, draggableId } = result;
+      if (!destination) return;
+      if (
+        destination.droppableId === source.droppableId &&
+        source.index === destination.index
+      )
+        return;
+
+      const column = data.columns[source.droppableId];
+      const newTaskIds = Array.from(column.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...column,
+        taskIds: newTaskIds,
+      };
+
+      const newData = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      setData(newData);
+    },
+    [data]
+  );
 
   return (
     <div className="App">
